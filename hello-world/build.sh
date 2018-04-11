ab6f004fd2a6ed1e2da022e378a0c069ca434e21
Tavish Armstrong <tavish@stripe.com>
2018-04-11 21:46:26 +0000

    update build.sh

--- hello-world/build.sh
+++ hello-world/build.sh
@@ -1,1 +1,6 @@
 docker build -t hello-node:v1 .
+docker tag hello-node:v1 us.gcr.io/tavish-200820/hello-node:v1
+gcloud auth configure-docker
+docker push us.gcr.io/tavish-200820/hello-node:v1
+
+gcloud container images list-tags us.gcr.io/tavish-200820/hello-node:v1
