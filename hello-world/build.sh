e483d7fd63c6182284326a453112ddf18a3165de
Tavish Armstrong <tavish@stripe.com>
2018-04-11 21:48:15 +0000

    remove tag

--- hello-world/build.sh
+++ hello-world/build.sh
@@ -3,4 +3,4 @@ docker tag hello-node:v1 us.gcr.io/tavish-200820/hello-node:v1
 gcloud auth configure-docker
 docker push us.gcr.io/tavish-200820/hello-node:v1
 
-gcloud container images list-tags us.gcr.io/tavish-200820/hello-node:v1
+gcloud container images list-tags us.gcr.io/tavish-200820/hello-node
