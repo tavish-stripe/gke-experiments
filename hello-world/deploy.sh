a925d289eede13f7fed5cfb0b21bacc0c487db7d
Tavish Armstrong <tavish@stripe.com>
2018-04-19 19:15:30 +0000

    fix hello-world istio setup

--- hello-world/deploy.sh
+++ hello-world/deploy.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl run hello-node --image=us.gcr.io/tavish-200820/hello-node:v1 --port=8080
-kubectl expose deployment hello-node --type=LoadBalancer
+kubectl apply -f hello.yaml
